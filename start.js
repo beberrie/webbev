06e6b4a6864355eee34fe08b3c0e127aec1bc330
beberrie <osbevericacabucos12@gmail.com>
2024-12-08 00:06:42 +0800

    Add files via upload

---
+++ start.js
@@ -0,0 +1,10 @@
+
+document.querySelector('.start-page').addEventListener('click', () => {
+  window.location.assign("home.html"); 
+});
+
+/*boutmefunc(){
+  window.location.href="start.html";
+}*/
+/*window.location.
+/*<button class = "name" onlick="boutmefunc()">*/
